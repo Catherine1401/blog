ae0c049c678e4c9811bef6c99444b8c592dcd5e4
Catherine1404 <huyvu.hw@gmail.com>
2025-04-29 18:08:50 +0700

    config

--- src/lib/config.js
+++ src/lib/config.js
@@ -5,8 +5,8 @@
  * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
  **/ 
 
-export const siteTitle = 'My Awesome Blog'
-export const siteDescription = 'Built with the SvelteKit Static Blog Starter'
+export const siteTitle = 'CodeLog'
+export const siteDescription = 'This is blog of Dung and HW'
 export const siteURL = 'example.com'
 export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
 export const siteAuthor = '- find and change this text in src/lib/config.js'
