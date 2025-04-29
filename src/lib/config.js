6f09f85cd3ec9819f5092cb1527136458c02f929
Catherine1404 <huyvu.hw@gmail.com>
2025-04-29 20:05:51 +0700

    rename author

--- src/lib/config.js
+++ src/lib/config.js
@@ -9,7 +9,7 @@ export const siteTitle = 'CodeLog'
 export const siteDescription = 'This is blog of Dung and HW'
 export const siteURL = 'example.com'
 export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
-export const siteAuthor = '- find and change this text in src/lib/config.js'
+export const siteAuthor = 'CodeLog'
 
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10
